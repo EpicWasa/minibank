c6ee6fb5ba38fdd996311ad993850065461b8b36
EpicWasa <mex.mackeev@gmail.com>
2020-05-18 20:19:07 +0300

    Heroku commit

--- src/main/java/ua/epicwasa/minibank/data/entity/Card.java
+++ src/main/java/ua/epicwasa/minibank/data/entity/Card.java
@@ -20,20 +20,14 @@ public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String number;
-
     private short secureKey;
-
     private LocalDate expirationDate;
-
     private LocalDate creationDate;
-
     public Card(Account account, String name) {
         this.account = account;
         this.name = name;
     }
-
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn( name = "account_id")
     private Account account;
